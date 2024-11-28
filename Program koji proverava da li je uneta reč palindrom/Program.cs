44747518522324a57d61469d1012190ba5625f3b
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 08:59:47 +0100

    Commit

---
+++ Program koji proverava da li je uneta reč palindrom/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite reč: ");
+        string rec = Console.ReadLine();
+
+        string obrnuto = new string(rec.ToCharArray().Reverse().ToArray());
+
+        if (rec.Equals(obrnuto, StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine($"Reč '{rec}' je palindrom.");
+        }
+        else
+        {
+            Console.WriteLine($"Reč '{rec}' nije palindrom.");
+        }
+    }
+}
